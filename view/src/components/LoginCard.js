9572929bb7005c2f44c547d0c3329d104b26e289
sbcool01 <swayambukharia02@gmail.com>
2021-01-08 14:57:18 +0530

    login page resolved error

--- view/src/components/LoginCard.js
+++ view/src/components/LoginCard.js
@@ -1,9 +1,6 @@
 import React from "react";
 import {Button, Grid, Typography, Avatar} from '@material-ui/core';
-import teamDetails from '../content/TeamDetails';
-import {microsoftAuth} from '../components/firebase/firebaseAuth';
 import { makeStyles } from '@material-ui/core/styles';
-import axios from "axios";
 import clubLogo from "../resources/icons/elecClubLogo.png"
 
 const useStyles = makeStyles((theme) => ({
@@ -33,35 +30,6 @@ function LoginCard(props) {
 
     const classes = useStyles();
 
-    let user={};
-    async function handleLogin() {
-        const response = await microsoftAuth();
-        if(response.isLoginSuccessful){
-            user = {
-                email : response.result.user.email,
-                name : response.result.user.displayName
-            };
-            await axios.post('http://localhost:4000/createUser', user)
-            .then((response) => {
-                user= {
-                    ...user,
-                    userId: response.data.userId
-                }
-                console.log("user: ", user);
-                localStorage.setItem('user', JSON.stringify(user));
-                teamDetails.forEach((teamPage) => {
-                    teamPage.forEach((teamMember) => {
-                        if(user.email === teamMember.email){
-                            localStorage.setItem('isAdmin', true);
-                        }
-                    })
-                })
-                
-                props.history.push('/user/'+ response.data.userId + '/projects');
-            })
-        }
-    }
-
     return (
         <Grid container className={classes.main}>
             <Grid item xs={12} className={classes.title}>
@@ -72,7 +40,7 @@ function LoginCard(props) {
                 <Typography variant="h5">Login and Apply for Projects</Typography>
             </Grid>
             <Grid item xs={12} className={classes.signInButton}>
-                <Button variant="contained" color="secondary" onClick={handleLogin}>Sign In IITG Outlook Id</Button>
+                <Button variant="contained" color="secondary" onClick={props.handleLogin}>Sign In IITG Outlook Id</Button>
             </Grid>
         </Grid>
     )
--- view/src/pages/Login.js
+++ view/src/pages/Login.js
@@ -5,6 +5,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppHeader from "../components/AppHeader";
 import AppFooter from "../components/AppFooter";
 import LoginCard from "../components/LoginCard";
+import teamDetails from '../content/TeamDetails';
+import {microsoftAuth} from '../components/firebase/firebaseAuth';
+import axios from "axios";
 
 const useStyles = makeStyles((theme) => ({
     backgroundDiv: {
@@ -41,36 +44,65 @@ const useStyles = makeStyles((theme) => ({
 function Login(props) {
     const classes = useStyles();
 
+    let user={};
+    async function handleLogin() {
+        const response = await microsoftAuth();
+        if(response.isLoginSuccessful){
+            user = {
+                email : response.result.user.email,
+                name : response.result.user.displayName
+            };
+            await axios.post('http://localhost:4000/createUser', user)
+            .then((response) => {
+                user= {
+                    ...user,
+                    userId: response.data.userId
+                }
+                console.log("user: ", user);
+                localStorage.setItem('user', JSON.stringify(user));
+                teamDetails.forEach((teamPage) => {
+                    teamPage.forEach((teamMember) => {
+                        if(user.email === teamMember.email){
+                            localStorage.setItem('isAdmin', true);
+                        }
+                    })
+                })
+                
+                props.history.push('/user/'+ response.data.userId + '/projects');
+            })
+        }
+    }
+
     return (
         <div>
             <AppHeader appBarTitle="ELECTRONICS CLUB IIT GUWAHATI"/>
             <div className={classes.backgroundDiv}>
                 <center>
-                <Grid container className={classes.main}>
-                    <Grid item xs = {12} md={7}>
-                        <Grid container spacing={2} className={classes.container}>
-                            <Grid item xs={12}>
-                                <Typography variant="h3" color="primary" style={{textAlign: 'center'}}>Welcome To The Community</Typography>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <Typography variant="h5"  color="primary" style={{textAlign: 'center'}}>WE BELIEVE IN EXCELLENCE</Typography>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                    <Hidden only={['xs', 'md', 'lg']}>
-                        <Grid item sm={3}></Grid>
-                    </Hidden>
-                    <Grid item xs={12} sm={6} md={5}>
-                        <Grid container spacing={2} className={classes.loginCard}>
-                            <Grid item xs={12}>
-                                <LoginCard />
+                    <Grid container className={classes.main}>
+                        <Grid item xs = {12} md={7}>
+                            <Grid container spacing={2} className={classes.container}>
+                                <Grid item xs={12}>
+                                    <Typography variant="h3" color="primary" style={{textAlign: 'center'}}>Welcome To The Community</Typography>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Typography variant="h5"  color="primary" style={{textAlign: 'center'}}>WE BELIEVE IN EXCELLENCE</Typography>
+                                </Grid>
                             </Grid>
                         </Grid>
+                        <Hidden only={['xs', 'md', 'lg']}>
+                            <Grid item sm={3}></Grid>
+                        </Hidden>
+                        <Grid item xs={12} sm={6} md={5}>
+                            <Grid container spacing={2} className={classes.loginCard}>
+                                <Grid item xs={12}>
+                                    <LoginCard handleLogin={handleLogin}/>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Hidden only={['xs', 'md', 'lg']}>
+                            <Grid item sm={3}></Grid>
+                        </Hidden>
                     </Grid>
-                    <Hidden only={['xs', 'md', 'lg']}>
-                        <Grid item sm={3}></Grid>
-                    </Hidden>
-                </Grid>
                 </center>
             </div>
             <AppFooter />
